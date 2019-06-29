a1266e3e822b5207c8dbbdcdf30740d5fe95a355
Pedro Henrique <pedrogamerlouco@outlook.com>
2019-06-29 10:39:57 -0300

    Added math funtions

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-module.exports = function(str) {
-    return str + ' - PDRLib!'
+function soma(n1, n2) {
+    return n1 + n2   
+}
+
+function subtr(n1, n2) {
+    return n1 - n2   
+}
+
+function multi(n1, n2) {
+    return n1 * n2   
+}
+
+function divi(n1, n2) {
+    return n1 / n2   
 }
